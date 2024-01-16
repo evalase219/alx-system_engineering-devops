d7cbe975307c4e5db0d0eb8c4899ccbe79fb4846
evalase219 <chiomaevangeline85@gmail.com>
2024-01-16 13:19:51 -0800

    How many sub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,10 +9,10 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+            "User-Agent": "Ubuntu:0x16.api.advanced:v1.0.0 (by u/Evalase85)"
             }
-    response = requests.get(url,  headers=headers,  allow_redirects=False)
-    if response.status_code == 404:
+    response = requests.get(url,  headers=headers)
+    if (not response.ok):
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    results = response.json().get("data").get("subscribers")
+    return results
